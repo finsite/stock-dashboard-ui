e65f4250f20bfc69cdbce046af21db7f14cf1e0a
Mark Quinn <mobious_99@yahoo.com>
2025-06-02 13:06:47 -0400

    fix: finalize Docusaurus config

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,156 +1,6 @@
-// import {themes as prismThemes} from 'prism-react-renderer';
-// import type {Config} from '@docusaurus/types';
-// import type * as Preset from '@docusaurus/preset-classic';
-
-// // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-// const config: Config = {
-//   title: 'Stock Dashboard UI',
-//   tagline: 'Modern stock tracking dashboard',
-//   favicon: 'img/favicon.ico',
-
-//   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-//   future: {
-//     v4: true, // Improve compatibility with the upcoming Docusaurus v4
-//   },
-
-//   // Set the production url of your site here
-//   url: 'https://finsite.github.io',
-//   // Set the /<baseUrl>/ pathname under which your site is served
-//   // For GitHub pages deployment, it is often '/<projectName>/'
-//   baseUrl: '/stock-dashboard-ui/',
-
-//   // GitHub pages deployment config.
-//   // If you aren't using GitHub pages, you don't need these.
-//   organizationName: 'finsite', // Usually your GitHub org/user name.
-//   projectName: 'stock-dashboard-ui', // Usually your repo name.
-
-//   onBrokenLinks: 'throw',
-//   onBrokenMarkdownLinks: 'warn',
-
-//   // Even if you don't use internationalization, you can use this field to set
-//   // useful metadata like html lang. For example, if your site is Chinese, you
-//   // may want to replace "en" with "zh-Hans".
-//   i18n: {
-//     defaultLocale: 'en',
-//     locales: ['en'],
-//   },
-
-//   presets: [
-//     [
-//       'classic',
-//       {
-//         docs: {
-//           sidebarPath: './sidebars.ts',
-//           // Please change this to your repo.
-//           // Remove this to remove the "edit this page" links.
-//           editUrl:
-//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-//         },
-//         blog: {
-//           showReadingTime: true,
-//           feedOptions: {
-//             type: ['rss', 'atom'],
-//             xslt: true,
-//           },
-//           // Please change this to your repo.
-//           // Remove this to remove the "edit this page" links.
-//           editUrl:
-//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-//           // Useful options to enforce blogging best practices
-//           onInlineTags: 'warn',
-//           onInlineAuthors: 'warn',
-//           onUntruncatedBlogPosts: 'warn',
-//         },
-//         theme: {
-//           customCss: './src/css/custom.css',
-//         },
-//       } satisfies Preset.Options,
-//     ],
-//   ],
-
-//   themeConfig: {
-//     // Replace with your project's social card
-//     image: 'img/docusaurus-social-card.jpg',
-//     navbar: {
-//       title: 'My Site',
-//       logo: {
-//         alt: 'My Site Logo',
-//         src: 'img/logo.svg',
-//       },
-//       items: [
-//         {
-//           type: 'docSidebar',
-//           sidebarId: 'tutorialSidebar',
-//           position: 'left',
-//           label: 'Tutorial',
-//         },
-//         {to: '/blog', label: 'Blog', position: 'left'},
-//         {
-//           href: 'https://github.com/facebook/docusaurus',
-//           label: 'GitHub',
-//           position: 'right',
-//         },
-//       ],
-//     },
-//     footer: {
-//       style: 'dark',
-//       links: [
-//         {
-//           title: 'Docs',
-//           items: [
-//             {
-//               label: 'Tutorial',
-//               to: '/docs/intro',
-//             },
-//           ],
-//         },
-//         {
-//           title: 'Community',
-//           items: [
-//             {
-//               label: 'Stack Overflow',
-//               href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-//             },
-//             {
-//               label: 'Discord',
-//               href: 'https://discordapp.com/invite/docusaurus',
-//             },
-//             {
-//               label: 'X',
-//               href: 'https://x.com/docusaurus',
-//             },
-//           ],
-//         },
-//         {
-//           title: 'More',
-//           items: [
-//             {
-//               label: 'Blog',
-//               to: '/blog',
-//             },
-//             {
-//               label: 'GitHub',
-//               href: 'https://github.com/facebook/docusaurus',
-//             },
-//           ],
-//         },
-//       ],
-//       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-//     },
-//     prism: {
-//       theme: prismThemes.github,
-//       darkTheme: prismThemes.dracula,
-//     },
-//   } satisfies Preset.ThemeConfig,
-// };
-
-// export default config;
 import { themes as prismThemes } from 'prism-react-renderer';
 import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
-
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
   title: 'Stock Dashboard UI',
@@ -163,7 +13,6 @@ const config: Config = {
 
   url: 'https://finsite.github.io',
   baseUrl: '/stock-dashboard-ui/',
-
   organizationName: 'finsite',
   projectName: 'stock-dashboard-ui',
 
@@ -181,8 +30,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          editUrl:
-            'https://github.com/finsite/stock-dashboard-ui/edit/main/docs/',
+          editUrl: 'https://github.com/finsite/stock-dashboard-ui/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -190,8 +38,7 @@ const config: Config = {
             type: ['rss', 'atom'],
             xslt: true,
           },
-          editUrl:
-            'https://github.com/finsite/stock-dashboard-ui/edit/main/docs/',
+          editUrl: 'https://github.com/finsite/stock-dashboard-ui/tree/main/',
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
           onUntruncatedBlogPosts: 'warn',
@@ -206,7 +53,7 @@ const config: Config = {
   themeConfig: {
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Stock Dashboard',
+      title: 'Stock Dashboard UI',
       logo: {
         alt: 'Stock Dashboard Logo',
         src: 'img/logo.svg',
@@ -216,7 +63,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         { to: '/blog', label: 'Blog', position: 'left' },
         {
@@ -231,12 +78,7 @@ const config: Config = {
       links: [
         {
           title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+          items: [{ label: 'Tutorial', to: '/docs/intro' }],
         },
         {
           title: 'Community',
@@ -258,10 +100,7 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            { label: 'Blog', to: '/blog' },
             {
               label: 'GitHub',
               href: 'https://github.com/finsite/stock-dashboard-ui',
@@ -269,7 +108,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Finsite. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Finsite.`,
     },
     prism: {
       theme: prismThemes.github,
